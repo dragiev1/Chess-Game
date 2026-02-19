25a24ab01e98ddd14eae8d6250f12ce227fc2a52
dragiev1 <dragievs2020@gmail.com>
2026-02-19 13:38:24 -0500

    feat: setup vue client and removed boilerplate

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import WelcomePage from './components/WelcomePage.vue';
+
+</script>
+
+<template>
+  <header>
+    <WelcomePage/>
+  </header>
+</template>
+
+<style scoped></style>
---
+++ client/src/components/WelcomePage.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>HELLO WELCOME TO CHYESS</header>
+</template>
+
+<style scoped></style>
